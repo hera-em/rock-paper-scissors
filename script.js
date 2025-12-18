7f0fe519f824137dcfcb339476a1ee597fbc9188
hera <165401313+hera-em@users.noreply.github.com>
2025-12-18 17:38:46 +0400

    Disable buttons upon loss or win

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let humanSelection = "";
 let computerSelection = "";
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
@@ -35,85 +36,92 @@ function playGame () {
     scissors.addEventListener('click', () => playRound("scissors", getComputerChoice()));
 
     function playRound (humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    switch (humanChoice) {
-        case "rock":
-            if (computerChoice === "scissors") {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("You win! Rock beats scissors")
-                results.appendChild(roundResult);
-                humanScore++;
-            }
-            else if (computerChoice === "paper") {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("You lose :( Paper beats rock")
-                results.appendChild(roundResult);
-                computerScore++;
-            }
-            else {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("It's a draw!")
-                results.appendChild(roundResult);
-            };
-            break;
+        humanChoice = humanChoice.toLowerCase();
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice === "scissors") {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("You win! Rock beats scissors")
+                    results.appendChild(roundResult);
+                    humanScore++;
+                }
+                else if (computerChoice === "paper") {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("You lose :( Paper beats rock")
+                    results.appendChild(roundResult);
+                    computerScore++;
+                }
+                else {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("It's a draw!")
+                    results.appendChild(roundResult);
+                };
+                break;
 
-        case "paper":
-            if(computerChoice === "scissors") {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("You lose :( Scissors beat paper!")
-                results.appendChild(roundResult);
+            case "paper":
+                if(computerChoice === "scissors") {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("You lose :( Scissors beat paper!")
+                    results.appendChild(roundResult);
 
-                computerScore++;
-            }
-            else if (computerChoice === "paper") {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("It's a draw!")
-                results.appendChild(roundResult);
+                    computerScore++;
+                }
+                else if (computerChoice === "paper") {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("It's a draw!")
+                    results.appendChild(roundResult);
 
-            }
-            else {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("You win! Paper beats rock!")
-                results.appendChild(roundResult);
-                humanScore++;
-            };
-            break;
+                }
+                else {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("You win! Paper beats rock!")
+                    results.appendChild(roundResult);
+                    humanScore++;
+                };
+                break;
 
-        case "scissors":
-            if (computerChoice === "scissors") {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("It's a draw!")
-                results.appendChild(roundResult);
+            case "scissors":
+                if (computerChoice === "scissors") {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("It's a draw!")
+                    results.appendChild(roundResult);
 
-            }
-            else if (computerChoice === "paper") {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("You win! Scissors beat paper")
-                results.appendChild(roundResult);
-                humanScore++;
-            }
-            else {
-                const roundResult = document.createElement("p");
-                roundResult.textContent = ("You lose :( Rock beats scissors")
-                results.appendChild(roundResult);
-                computerScore++;
-            };
-            break;
-    }
-    humanScoreResult.textContent = `Your score: ${humanScore}`; 
-    computerScoreResult.textContent = `Computer score: ${computerScore}`;
+                }
+                else if (computerChoice === "paper") {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("You win! Scissors beat paper")
+                    results.appendChild(roundResult);
+                    humanScore++;
+                }
+                else {
+                    const roundResult = document.createElement("p");
+                    roundResult.textContent = ("You lose :( Rock beats scissors")
+                    results.appendChild(roundResult);
+                    computerScore++;
+                };
+                break;
+        }
+        humanScoreResult.textContent = `Your score: ${humanScore}`; 
+        computerScoreResult.textContent = `Computer score: ${computerScore}`;
 
-    if (humanScore == 5) {
-        const winner = document.createElement("p");
-        winner.textContent = "And the winner is... you!";
-        results.appendChild(winner);
-    }
 
-    else if (computerScore == 5) {
-        const winner = document.createElement("p");
-        winner.textContent = "And the winner is... the computer!";
-        results.appendChild(winner);
-    }
+        if (humanScore == 5) {
+            const winner = document.createElement("p");
+            winner.textContent = "And the winner is... you!";
+            results.appendChild(winner);
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+        }
+
+        else if (computerScore == 5) {
+            const winner = document.createElement("p");
+            winner.textContent = "And the winner is... the computer!";
+            results.appendChild(winner);
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+        }
 }
 
 
